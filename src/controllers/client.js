085e71c933ab58ff5387eae6cea3d07e4c63e9d6
Gustavo <gustavommh@gmail.com>
2019-03-12 10:38:42 -0300

    client search

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -1,4 +1,5 @@
-const { Client } = require ('../models');
+const { Client } = require('../models');
+const Sequelize = require('sequelize');
 // const {gerarToken} = require('../utils/token');
 // const bcrypt = require('bcryptjs');
 
@@ -6,154 +7,169 @@ const { Client } = require ('../models');
     lista todos os clientes ativos com status = 1,
     que é o default
 */
-function findAll (request, response, next){
-    Client.findAll({ where: { status: 1} })
-    .then(client => {
-        if (!client){
-            response.status(404).send('cliente não encontrado')
-        }else{
-            response.status(200).json(client);
-        }
-    })
-    .catch(ex=>{
-        console.error(ex)
-        response.status(412).send('não foi possível consultar o banco de dados')
-    })
+function findAll(request, response, next) {
+
+    const { query: { search } } = request;
+
+    const clientQuery = {
+        where: { status: 1 }
+    }
+
+    if (search) {
+        clientQuery.where.name = {
+            [Sequelize.Op.like]: `%${search}%`
+        },
+            clientQuery.where.status = 1
+    }
+
+    Client.findAll(clientQuery)
+        .then(client => {
+            if (!client) {
+                response.status(404).send('cliente não encontrado')
+            } else {
+                response.status(200).json(client);
+            }
+        })
+        .catch(ex => {
+            console.error(ex)
+            response.status(412).send('não foi possível consultar o banco de dados')
+        })
 }
 
 /*
     lista todos os clientes inativos com status = 0, 
     que foram desativados por algum motivo
 */
-function findAllDisable (request, response, next){
-    Client.findAll({ where: { status: 0} })
-    .then(client => {
-        if (!client){
-            response.status(404).send('cliente não encontrado')
-        }else{
-            response.status(200).json(client);
-        }
-    })
-    .catch(ex=>{
-        console.error(ex)
-        response.status(412).send('não foi possível consultar o banco de dados')
-    })
+function findAllDisable(request, response, next) {
+    Client.findAll({ where: { status: 0 } })
+        .then(client => {
+            if (!client) {
+                response.status(404).send('cliente não encontrado')
+            } else {
+                response.status(200).json(client);
+            }
+        })
+        .catch(ex => {
+            console.error(ex)
+            response.status(412).send('não foi possível consultar o banco de dados')
+        })
 }
 
 
 //  registra o cliente
 function register(request, response, next) {
 
-    const { body:{ name, end, birth, tel, email, type, doc1, doc2 } } = request
+    const { body: { name, end, birth, tel, email, type, doc1, doc2 } } = request
 
     // const salt = bcrypt.genSaltSync(10);
     // const senha2 = bcrypt.hashSync(senha, salt);
 
     Client.create({
-        name, end, birth, tel, email, type, doc1, doc2})
-    .then( client => {
-        response.status(201).json(client)
+        name, end, birth, tel, email, type, doc1, doc2
     })
-    .catch( e => {
-        console.error(e);
-        response.status(412).send('não foi possível incluir o registro')
-    })
+        .then(client => {
+            response.status(201).json(client)
+        })
+        .catch(e => {
+            console.error(e);
+            response.status(412).send('não foi possível incluir o registro')
+        })
 }
 
 // lista os dados de um cliente pelo seu ID
 function findById(request, response, next) {
-   
-    const { params:{clientId} } = request
+
+    const { params: { clientId } } = request
 
     Client.findById(clientId)
-    .then(client => {
-        if (!client){
-            response.status(404).send('usuário não encontrado')
-        }else{
-            response.status(200).json(client);
-        }
-    })
-    .catch(ex=>{
-        console.error(ex)
-        response.status(412).send('não foi possível consultar o banco de dados')
-    })
+        .then(client => {
+            if (!client) {
+                response.status(404).send('usuário não encontrado')
+            } else {
+                response.status(200).json(client);
+            }
+        })
+        .catch(ex => {
+            console.error(ex)
+            response.status(412).send('não foi possível consultar o banco de dados')
+        })
 
 }
 
 // atualiza os dados do cliente pelo seu ID
 function update(request, response, next) {
 
-    const {params:{clientId}, body:{name, end, birth, tel, email, type, doc1, doc2}} = request
+    const { params: { clientId }, body: { name, end, birth, tel, email, type, doc1, doc2 } } = request
 
     Client.findById(clientId)
-    .then( i => {
-        if (!i){
-            response.status(404).send('cliente não encontrado')
-        }else{
-            return i.update({ name, end, birth, tel, email, type, doc1, doc2 })
-            .then(()=>{
-                response.status(200).json(i)
-            })
-        }
-    })
-    .catch(ex=>{
-        console.error(ex)
-        response.status(412).send('não foi possível consultar o banco de dados')
-    })
+        .then(i => {
+            if (!i) {
+                response.status(404).send('cliente não encontrado')
+            } else {
+                return i.update({ name, end, birth, tel, email, type, doc1, doc2 })
+                    .then(() => {
+                        response.status(200).json(i)
+                    })
+            }
+        })
+        .catch(ex => {
+            console.error(ex)
+            response.status(412).send('não foi possível consultar o banco de dados')
+        })
 }
 
 // desativa o cliente (status=0) pelo seu ID
 function disable(request, response, next) {
 
-    const { params:{clientId} } = request
+    const { params: { clientId } } = request
     let status = 0;
 
     Client.findById(clientId)
-    .then( i => {
-        if (!i){
-            response.status(404).send('cliente não encontrado')
-        }else{
-            return i.update({ status })
-            .then(()=>{
-                response.status(200).send('cliente desativado')
-                console.log(i);
-            })
-        }
-    })
-    .catch(ex=>{
-        console.error(ex)
-        response.status(412).send('não foi possível consultar o banco de dados')
-    })
+        .then(i => {
+            if (!i) {
+                response.status(404).send('cliente não encontrado')
+            } else {
+                return i.update({ status })
+                    .then(() => {
+                        response.status(200).send('cliente desativado')
+                        console.log(i);
+                    })
+            }
+        })
+        .catch(ex => {
+            console.error(ex)
+            response.status(412).send('não foi possível consultar o banco de dados')
+        })
 
 }
 
 // ativa o cliente (status=1) pelo seu ID
 function enable(request, response, next) {
 
-    const { params:{clientId} } = request
+    const { params: { clientId } } = request
     let status = 1;
 
     Client.findById(clientId)
-    .then( i => {
-        if (!i){
-            response.status(404).send('cliente não encontrado')
-        }else{
-            return i.update({status})
-            .then(()=>{
-                response.status(200).json(i);
-                console.log(i);
-            })
-        }
-    })
-    .catch(ex=>{
-        console.error(ex)
-        response.status(412).send('não foi possível consultar o banco de dados')
-    })
+        .then(i => {
+            if (!i) {
+                response.status(404).send('cliente não encontrado')
+            } else {
+                return i.update({ status })
+                    .then(() => {
+                        response.status(200).json(i);
+                        console.log(i);
+                    })
+            }
+        })
+        .catch(ex => {
+            console.error(ex)
+            response.status(412).send('não foi possível consultar o banco de dados')
+        })
 
 }
 
 // function login(request, response, next) {
-    
+
 //     const {body:{ email, senha }} = request;
 
 //     Usuario.findOne({
